a6afc925cb1ec6bdec706da54be003150b7540a9
pavelFaraday <devstudio.corp@gmail.com>
2021-09-12 13:50:13 +0400

    React Redux Projects - 2. Simple Project | fetch data from Rest API

--- simple-project/src/components/HomePage/constants.js
+++ simple-project/src/components/HomePage/constants.js
@@ -1,3 +1,3 @@
-const ActionTypes = {
+export const ActionTypes = {
 	SET_USERS: "app/components/HomePage/SET_USERS",
 };
--- simple-project/src/components/HomePage/reducers.js
+++ simple-project/src/components/HomePage/reducers.js
@@ -1,9 +1,13 @@
+import { ActionTypes } from "./constants";
+
 const defaultState = {
-	users: ["No users!"],
+	users: [],
 };
 
 const homePageReducer = (state = defaultState, action) => {
 	switch (action.type) {
+		case ActionTypes.SET_USERS:
+			return { ...state, users: action.payload };
 		default:
 			return state;
 	}
